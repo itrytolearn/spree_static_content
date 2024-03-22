b5edfbcf62069184d63ba011f748ce4f0297a123
itrytolearn <manu_cercelaru@yahoo.com>
2024-03-22 12:32:39 +0200

    Deleted show_on_intranet from translated attributes

--- app/controllers/spree/admin/pages_controller.rb
+++ app/controllers/spree/admin/pages_controller.rb
@@ -17,7 +17,7 @@ module Spree
   end
 
       def permitted_params
-        [:translations_attributes => [:id, :title, :body, :slug, :layout, :foreign_link, :meta_keywords, :meta_title, :meta_description, :show_on_intranet]]
+        [:translations_attributes => [:id, :title, :body, :slug, :layout, :foreign_link, :meta_keywords, :meta_title, :meta_description]]
       end
     end
   end
